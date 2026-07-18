f797a82c9d6ad1cae3904a999773f807f7a81eed
niturekha-26 <biswalnitu240@gmail.com>
2026-07-18 13:15:06 +0530

    Completed Flask User Authentication System

---
+++ init_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database created successfully!")
